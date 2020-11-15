e43c9c2b60011f2bfc74d31fed01798b5842eb43
Luke D'Alessandro <ldalessa@uw.edu>
2020-11-15 14:05:11 -0800

    Move some old files around.

--- include/ttl/ttl.hpp
+++ include/ttl/ttl.hpp
@@ -1,20 +1,7 @@
 #pragma once
 
-#include "Equation.hpp"
 #include "Index.hpp"
 #include "Tensor.hpp"
 #include "System.hpp"
 #include "dot.hpp"
 #include "grammar.hpp"
-
-// #include "Index.hpp"
-// #include "RPNTensorTree.hpp"
-// #include "ScalarSystem.hpp"
-// #include "System.hpp"
-// #include "Tensor.hpp"
-// #include "TensorTree.hpp"
-// #include "dot.hpp"
-
-// #include "Rational.hpp"
-// #include "ScalarSystem.hpp"
-// #include "TaggedTree.hpp"
